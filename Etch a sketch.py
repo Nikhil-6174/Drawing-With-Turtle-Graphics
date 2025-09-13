bae1a24f09f48ef3466e9f102d230cb7ef0f83e4
Nikhil-6174 <manipal.nikku@gmail.com>
2025-09-13 12:41:33 +0530

    Add files via upload
    Use caps W,S,A,D for drawing forward, backward, left and right.

---
+++ Etch a sketch.py
@@ -0,0 +1,37 @@
+from turtle import Turtle, Screen
+
+
+
+tim = Turtle()
+tim.color("Green")
+tim.pencolor("Blue")
+tim.shape("circle")
+tim.pensize(6)
+
+def move_fw():
+    tim.fd(20)
+
+def move_bk():
+    tim.bk(20)
+
+def turn_rt():
+    tim.rt(25)
+
+def turn_lt():
+    tim.lt(25)
+
+def clear():
+    tim.home()
+    tim.clear()
+
+screen = Screen()
+screen.listen()
+screen.onkeypress(fun=move_fw,key='W')
+screen.onkeypress(fun=move_bk,key='S')
+screen.onkeypress(fun=turn_lt,key='A')
+screen.onkeypress(fun=turn_rt,key='D')
+screen.onkeypress(fun=clear,key='C')
+
+screen.title("Welcome to etch a drawing!")
+screen.bgcolor('Black')
+screen.exitonclick()
